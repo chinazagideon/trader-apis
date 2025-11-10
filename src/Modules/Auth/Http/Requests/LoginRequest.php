02da28b3c077c5c6ee78c23deaa11739390d7d39
chinazagideon <chinazagideon264@gmail.com>
2025-11-11 00:24:53 +0100

    improve route, configure client

--- src/Modules/Auth/Http/Requests/LoginRequest.php
+++ src/Modules/Auth/Http/Requests/LoginRequest.php
@@ -22,11 +22,26 @@ class LoginRequest extends FormRequest
             'email' => 'required|email',
             'password' => 'required|string',
             'guard' => 'sometimes|string|in:sanctum,jwt,api',
+            'api_key' => 'sometimes|string|max:255|exists:clients,api_key',
+
         ];
     }
 
+
     /**
-     * Undocumented function
+     * add api key from header or input
+     *
+     * @return void
+     */
+    public function prepareForValidation()
+    {
+        $this->merge([
+            'api_key' => $this->header('X-Client-Api-Key', $this->input('api_key')),
+        ]);
+    }
+
+    /**
+     * validation messages
      *
      * @return array
      */
@@ -37,6 +52,8 @@ class LoginRequest extends FormRequest
             'email.email' => 'The email must be a valid email address.',
             'password.required' => 'The password field is required.',
             'guard.in' => 'The guard must be one of: sanctum, jwt, api.',
+            'api_key.sometimes' => 'The API key field is required.',
+            'api_key.exists' => 'The API key does not exist',
         ];
     }
 }
