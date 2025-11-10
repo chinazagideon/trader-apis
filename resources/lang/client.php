3a10b34e970eb70ccb3ce840c4f1351937ca1d34
chinazagideon <chinazagideon264@gmail.com>
2025-11-11 00:25:35 +0100

    add lang support

---
+++ resources/lang/client.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'unauthorized' => 'Unauthorized',
+
+    'client_id_set' => 'client_id',
+    
+    'current_id_set' => "current_client_id",
+    'current_client_set' => "current_client",
+    'current_client_id_not_set' => "current_client_id not set",
+    'current_client_not_set' => "current_client not set",
+    'current_client_not_found' => "current_client not found",
+    'current_client_not_found' => "current_client not found",
+];
---
+++ resources/lang/payment.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'payment_gateway' => [
+        'instructions' => [
+            'title' => 'Please send your payment to the following address: <address>',
+        ]
+    ]
+];
